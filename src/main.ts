2ae61d337f6078449e8461b1247c59c124090ad7
dxsixpc <dxsixpc@gmail.com>
2023-09-01 22:21:29 +0800

    feat: 去除水印及校验

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,13 @@ import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
 
+import { LicenseManager } from '@ag-grid-enterprise/core'
+LicenseManager.prototype.validateLicense = function () {}
+LicenseManager.prototype.isDisplayWatermark = () => false
+
+
 const app = createApp(App)
 app.use(ElementPlus)
 app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
